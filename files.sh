9121ca55ceec442b57603efa9aef999398ed3d7f
Alexander Zharinov <aleksandr.zharinov@saprun.com>
2016-06-20 21:15:41 +0300

    Script to get ans set configs

---
+++ files.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Collect all my dotfiles into place.
+
+FILES=(
+    '.tmux.conf'
+    '.vimrc'
+    '.bashrc'
+    '.gitignore'
+)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILES_DIR="$SCRIPT_DIR/files"
+HOME_DIR="$(realpath ~)"
+
+exit_usage() {
+    echo 'Usage:'
+    echo '    ./files.sh get      Collect dotfiles from my home into directory "files/".'
+    echo '    ./files.sh set      Get dotfiles from directory "files/" ans install into'
+    echo '                        my home.'
+    exit 1
+}
+
+exit_on_file() {
+    echo "Error while processing file $1"
+    exit 1
+}
+
+get_func() {
+    for F in ${FILES[*]}
+    do
+        CMD="cp $HOME_DIR/$F $FILES_DIR/$F"
+        echo "$CMD"
+        $CMD || exit_on_file "$F"
+    done
+}
+
+set_func() {
+    for F in ${FILES[*]}
+    do
+        CMD="cp $FILES_DIR/$F $HOME_DIR/$F"
+        echo "$CMD"
+        $CMD || exit_on_file "$F"
+    done
+}
+
+main() {
+    case "$1" in
+        'get')
+            get_func
+            ;;
+        'set')
+            set_func
+            ;;
+        *)
+            exit_usage
+            ;;
+    esac
+}
+
+main $@
